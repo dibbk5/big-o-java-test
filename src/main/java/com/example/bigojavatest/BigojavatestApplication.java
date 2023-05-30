4876e3860c37c24e150bde965940ca720b3e1c01
Koy Dibb <dibbk5@gmail.com>
2023-05-30 11:05:51 -0600

    Runtime

--- src/main/java/com/example/bigojavatest/BigojavatestApplication.java
+++ src/main/java/com/example/bigojavatest/BigojavatestApplication.java
@@ -8,6 +8,12 @@ public class BigojavatestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BigojavatestApplication.class, args);
+
+		int[] newArray = new int[]{1,2,3,4,5,6,7,9,10};
+
+		System.out.println(MissingNumber.missingNumber(newArray, 10));
+		System.out.println(MissingNumber.missingNumberSort(newArray, 10));
+		System.out.println(MissingNumber.missingNumberSum(newArray, 10));
 	}
 
 }
--- src/main/java/com/example/bigojavatest/MissingNumber.java
+++ src/main/java/com/example/bigojavatest/MissingNumber.java
@@ -1,7 +1,48 @@
 package com.example.bigojavatest;
 
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class MissingNumber {
-    int[] newArray = new int[]{1,2,3,4,5,6,7,9,10};
+
+    public static int missingNumber(int[] nums, int maxNum) {
+        Set<Integer> set = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+
+        for (int i = 1; i <= maxNum; i += 1) {
+            if (!set.contains(i)) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    public static int missingNumberSort(int[] nums, int maxNum) {
+        Arrays.sort(nums);
+
+        for (int i = 1; i < maxNum; i += 1) {
+            if (i != nums[i-1]) {
+                return i;
+            }
+        }
+
+        if (nums.length == maxNum && nums [maxNum - 1] == maxNum) {
+            return 0;
+        }
+
+        return maxNum;
+    }
+
+    public static int missingNumberSum(int[] nums, int maxNum) {
+        int expectedSum = 0;
+        for (int i = 1; i <= maxNum; i += 1) {
+            expectedSum += i;
+        }
+
+        int actualSum = Arrays.stream(nums).sum();
+
+        return expectedSum - actualSum;
+    }
 
 
 
